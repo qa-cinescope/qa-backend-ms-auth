447a40f0c48a01c7e07b54d534817e778160798d
ZeBolHD <zezebolz@gmail.com>
2024-04-24 20:49:50 +0300

    feat: added server to swagger

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,14 @@ import { Logger } from "nestjs-pino";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const HOST = configService.get<string>("HOST_AUTH_URL");
 
   app.useLogger(app.get(Logger));
 
@@ -19,15 +24,16 @@ async function bootstrap() {
   app.use(cookieParser());
 
   const config = new DocumentBuilder()
-    .setTitle("Auth api")
-    .setDescription("This api for auth")
-    .setVersion("1.02")
+    .setTitle("Auth API")
+    .setDescription("This API for auth")
+    .setVersion("1.02.1")
+    .addServer(HOST, "API server")
     .setExternalDoc("Коллекция json", "/swagger-json")
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("swagger", app, document);
+  SwaggerModule.setup("swagger", app, document, {});
 
   await app.listen(5500);
 }
